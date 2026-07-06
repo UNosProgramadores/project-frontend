8c7a53d37789e484ac2ecd38b64378a899b96c31
NIcolas Aguirre <nicolas.aguirre2k7@gmail.com>
2026-07-05 19:41:34 -0500

    feat: change base view and add new ones tot he public.routes

--- src/router/routes/public.routes.js
+++ src/router/routes/public.routes.js
@@ -1,9 +1,14 @@
 import LoginView from '@/views/public/LoginView.vue'
 import RegisterView from '@/views/public/RegisterView.vue'
-import ParkingMapView from '@/views/public/ParkingMapView.vue'
+import ParkingLotListView from '@/views/public/ParkingLotListView.vue'
 
 export default [
-  { path: '/', name: 'map', component: ParkingMapView },
+  { path: '/', name: 'home', component: ParkingLotListView },
   { path: '/login', name: 'login', component: LoginView },
-  { path: '/register', name: 'register', component: RegisterView }
+  { path: '/register', name: 'register', component: RegisterView },
+  {
+    path: '/map/:parkingLotId',
+    name: 'parking-map',
+    component: () => import('@/views/public/ParkingMapView.vue')
+  }
 ]

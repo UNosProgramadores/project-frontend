e4ae5ae1afdfbd823ce7abeb485a716ce01b9464
NIcolas Aguirre <nicolas.aguirre2k7@gmail.com>
2026-07-06 19:18:05 -0500

    feat: add topbar to main app

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
-<script setup></script>
+<script setup>
+import AppTopBar from '@/components/layout/AppTopBar.vue'
+</script>
 
 <template>
-  <router-view />
+  <AppTopBar />
+  <main class="main-content">
+    <router-view />
+  </main>
 </template>
+
+<style>
+body {
+  margin: 0;
+}
+.main-content {
+  padding-top: 64px;
+}
+</style>

4b18ecf480468d585875949364fda39a548fb397
NIcolas Aguirre <nicolas.aguirre2k7@gmail.com>
2026-07-06 21:29:51 -0500

    feat: add site bar to the main app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,32 @@
 <script setup>
 import AppTopBar from '@/components/layout/AppTopBar.vue'
+import AppSidebar from '@/components/layout/AppSidebar.vue'
+import { useAuthStore } from '@/stores/auth'
+
+const authStore = useAuthStore()
 </script>
 
 <template>
   <AppTopBar />
-  <main class="main-content">
-    <router-view />
-  </main>
+  <div class="app-layout">
+    <AppSidebar v-if="authStore.isAuthenticated" />
+    <main class="main-content">
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <style>
 body {
   margin: 0;
 }
+.app-layout {
+  display: flex;
+  min-height: calc(100vh - 56px);
+  margin-top: 56px;
+}
 .main-content {
-  padding-top: 64px;
+  flex: 1;
+  padding: 1.25rem;
 }
 </style>

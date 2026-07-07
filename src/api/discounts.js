40d6d03e11c3f43f1b887b78bb63931b6ee90eb4
NIcolas Aguirre <nicolas.aguirre2k7@gmail.com>
2026-07-07 17:30:04 -0500

    feat: handle new http endpoints calls

--- src/api/discounts.js
+++ src/api/discounts.js
@@ -1,12 +1,15 @@
 import http from './http'
 
-export const getDiscounts = (parkingLotId) => http.get(`/parking-lots/${parkingLotId}/discounts`)
+export const getDiscounts = (parkingLotId) => http.get(`/parking-lots/${parkingLotId}/discounts/config`)
 
 export const createDiscount = (parkingLotId, data) =>
-  http.post(`/parking-lots/${parkingLotId}/discounts`, data)
+  http.post(`/parking-lots/${parkingLotId}/discounts/config`, data)
 
 export const updateDiscount = (parkingLotId, discountId, data) =>
-  http.put(`/parking-lots/${parkingLotId}/discounts/${discountId}`, data)
+  http.put(`/parking-lots/${parkingLotId}/discounts/config/${discountId}`, data)
 
 export const deleteDiscount = (parkingLotId, discountId) =>
-  http.delete(`/parking-lots/${parkingLotId}/discounts/${discountId}`)
+  http.delete(`/parking-lots/${parkingLotId}/discounts/config/${discountId}`)
+
+export const toggleDiscountsEnabled = (parkingLotId) =>
+  http.patch(`/parking-lots/${parkingLotId}/discounts/toggle`)
--- src/api/reports.js
+++ src/api/reports.js
@@ -1,7 +1,4 @@
 import http from './http'
 
-export const getReportSummary = (parkingLotId, params = {}) =>
-  http.get(`/parking-lots/${parkingLotId}/reports/summary`, { params })
-
-export const getDailyReport = (parkingLotId, params = {}) =>
-  http.get(`/parking-lots/${parkingLotId}/reports/daily`, { params })
+export const getReport = (parkingLotId, params = {}) =>
+  http.get(`/parking-lots/${parkingLotId}/reports`, { params })
